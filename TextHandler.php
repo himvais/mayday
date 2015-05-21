fb05ed4f123d0e9004f4924425fbb9f5d1b688ef
Himanshu <himanshu@MacBook-Air.local>
2015-05-21 13:39:26 +0530

    Added the new file - TextHandler

---
+++ TextHandler.php
@@ -0,0 +1,19 @@
+<!-- Project Mayday -->
+<!-- FILE - CORE-TextHangler V0.1(Base:PHP) -->
+<!-- Written by @Himvais on 21/05/2015 -->
+
+<!-- this file contains the classes required to handle the input type text. -->
+<!-- Text handler for the phase one just tries to communicate with the ChatScript Server and gets back the response. -->
+<?php 
+	/**
+	* The Core class which provide methods for commuicating with the ChatScript server.
+	*/
+	class ClassName extends AnotherClass
+	{
+		
+		function __construct(argument)
+		{
+			# code...
+		}
+	}
+ ?>
